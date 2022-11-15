90ef70693f61c55fd92862fd52574c4ca7633a20
pavel777666 <goszakupki.aktiv@gmail.com>
2022-11-15 05:11:49 +0300

    Modul 2 PP

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext =     //обращаемся к объектам интерфейса спринг приложения (чарез наследование)
+                new AnnotationConfigApplicationContext(AppConfig.class);  //запускаем поиск аннотаций
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");  //бин сингелтона создан при инициализации конструктора
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");  //ссылка та же самая, так как сингелтон
+        System.out.println(bean.getMessage());      //выводим в консоль ссылку на объект bean
+        System.out.println(bean1.getMessage());     //выводим в консоль ту же ссылку объект bean
+        Cat cat = (Cat) applicationContext.getBean("cat");  //инициализируем объект Бина cat
+        Cat cat1 = (Cat) applicationContext.getBean("cat"); //инициализируем объект второго Бина cat1
+        System.out.println(cat.getName());                      //выводим в консоль строку(поле) первого объекта Бина кэт
+        System.out.println(cat1.getName());                      //выводим в консоль строку(поле) второго объекта Бина кэт1
+        System.out.println(bean == bean1);                      //сравниваем равные ссылки bean==bean1
+        System.out.println(cat == cat1);                        //сравниваем ссылки на разные участки памяти у Бинов класса Cat
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -6,9 +6,20 @@ import org.springframework.context.annotation.Scope;
 public class AppConfig {
  
     @Bean(name="helloworld")
+    @Scope ("singleton")    //сингелтон по умолчанию и можно не писать. может быть 1, имя можно перезаписать через сеттер
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name = "cat")
+    @Scope("prototype")  //создает бин только при обращении, каждый раз новый в новом участке памяти
+    public Cat getCat(){
+        Cat cat = new Cat();
+        cat.setName("Cat !");
+        return cat;
+    }
+
+
 }
